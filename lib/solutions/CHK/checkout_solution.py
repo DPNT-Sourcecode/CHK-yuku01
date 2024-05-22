dc9ab08e89725cc69014eb8b30f382db2ed0482a
sbx_user1051 <sbx_user1051@169.254.104.213>
2024-05-22 12:13:29 +0000

    Wed May 22 12:13:29 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,17 +1,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
-
+import logging
 
 # DB tables mock
 PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-OFFERS = {'A': {3: 130}, 'B': {2: 45}}
+OFFERS = {
+    'A': {'count': 3, 'price': 130},
+    'B': {'count': 2, 'price': 45},
+}
 
 
 def checkout(skus: str) -> int:
     if not skus.isalpha() or not skus.isupper():
         return -1
+
     amount = 0
-    for item in
+    for item in set(skus):
+        if item not in PRICES:
+            logging.warning(f'Product {item} not in DB')
+            return -1
+
+        products_n = skus.count(item)
+        discount_price = 0
+        if item in OFFERS:
+            discount_price = (
+                (products_n // OFFERS[item]['count']) * OFFERS[item]['price']
+            )
+            products_n = products_n % OFFERS[item]['count']
+
+        amount += PRICES[item] * products_n + discount_price
 
     return amount
+
 
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,26 @@
+import pytest
+
+from lib.solutions.CHK.checkout_solution import checkout
+
+
+@pytest.mark.parametrize(
+    'products,output',
+    (
+        ('B', 30),
+        ('ABCD', 115),
+        ('AAA', 130),
+        ('AAAAAA', 260),
+        ('AAAA', 180),
+        ('AAABB', 180),
+    ),
+)
+def test_checkout_success(products, output):
+    assert checkout(products) == output
+
+
+@pytest.mark.parametrize(
+    'products',
+    ('E', 'ab', 'AB4', 'A B C D', 4),
+)
+def test_checkout_error(products):
+    assert checkout(products) == -1
