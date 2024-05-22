d80bc79deb41a64a3f8963199467e0e77994acc5
sbx_user1051 <sbx_user1051@169.254.104.213>
2024-05-22 11:17:38 +0000

    Wed May 22 11:17:38 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,3 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
     return x + y
-
-
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,4 +5,3 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
         assert sum_solution.compute(0, 0) == 0
-
