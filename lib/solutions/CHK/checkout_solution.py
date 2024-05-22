738aa01216857b3524b89f79530895233d94586b
sbx_user1051 <sbx_user1051@169.254.104.213>
2024-05-22 11:52:29 +0000

    Wed May 22 11:52:29 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,17 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+
+
+# DB tables mock
+PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+OFFERS = {'A': {3: 130}, 'B': {2: 45}}
+
+
+def checkout(skus: str) -> int:
+    if not skus.isalpha() or not skus.isupper():
+        return -1
+    amount = 0
+    for item in
+
+    return amount
+
